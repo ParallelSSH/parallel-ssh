3762fc23126f333b9390ecbee655f1b17871b6ac
Panos <pkittenis@gmail.com>
2014-02-19 11:05:23 +0000

    Bump version for bug fix release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='parallel-ssh',
-      version='0.1',
+      version='0.2',
       description='Wrapper library over paramiko to allow remote execution of tasks. Supports parallel execution on multiple hosts',
       author='Panos Kittenis',
       author_email='pkittenis@gmail.com',
