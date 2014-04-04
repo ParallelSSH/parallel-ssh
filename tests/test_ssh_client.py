193ae31d3f1bfe16ee5dd802b204e4aac0b08e04
Panos <pkittenis@gmail.com>
2014-04-04 15:33:27 +0100

    Added sftp server interface stub by https://github.com/rspivak/sftpserver. Added stub SFTP handler to fake server. Added SFTP functionality tests for SSHClient. Cleaned up fake server startup, doc changes

--- tests/test_ssh_client.py
+++ tests/test_ssh_client.py
@@ -3,34 +3,50 @@
 """Unittests for parallel-ssh"""
 
 import unittest
-from pssh import SSHClient, ParallelSSHClient, UnknownHostException
-from paramiko import AuthenticationException
+from pssh import SSHClient, ParallelSSHClient, UnknownHostException, AuthenticationException, _setup_logger, logger
+from fake_server.fake_server import start_server, make_socket, logger as server_logger, \
+    paramiko_logger
+import os
+
+# _setup_logger(server_logger)
+# _setup_logger(logger)
+# _setup_logger(paramiko_logger)
 
 class SSHClientTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.fake_cmd = 'fake cmd'
+        self.fake_resp = 'fake response'
+        self.listen_socket = make_socket('127.0.0.1')
+        self.listen_port = self.listen_socket.getsockname()[1]
 
-    def test_ssh_client(self):
-        try:
-            client = SSHClient('testy')
-        except UnknownHostException, e:
-            print e
-            return
-        stdin, stdout, stderr = client.exec_command('ls -ltrh')
-        for line in stdout:
-            print line.strip()
-        client.copy_file("fake file", "fake file")
-
-class ParallelSSHClientTest(unittest.TestCase):
-
-    def test_parallel_ssh_client(self):
-        client = ParallelSSHClient(['testy'])
-        cmds = client.exec_command('ls -ltrh')
-        try:
-            print [client.get_stdout(cmd) for cmd in cmds]
-        except UnknownHostException, e:
-            print e
-            return
-        cmds = client.copy_file('fake file', 'fake file')
-        client.pool.join()
+    def test_ssh_client_sftp(self):
+        """Test SFTP features of SSHClient. Copy local filename to server,
+        check that data in both files is the same, make new directory on
+        server, remove files and directory."""
+        test_file_data = 'test'
+        local_filename = 'test_file'
+        remote_filename = 'test_file_copy'
+        remote_dir = 'remote_dir'
+        test_file = open(local_filename, 'w')
+        test_file.writelines([test_file_data + os.linesep])
+        test_file.close()
+        server = start_server({ self.fake_cmd : self.fake_resp }, self.listen_socket)
+        client = SSHClient('127.0.0.1', port=self.listen_port)
+        client.copy_file(local_filename, remote_filename)
+        self.assertTrue(os.path.isfile(remote_filename), msg="SFTP copy failed")
+        copied_file = open(remote_filename, 'r')
+        copied_file_data = copied_file.readlines()[0].strip()
+        copied_file.close()
+        self.assertEqual(test_file_data, copied_file_data,
+                         msg="Data in destination file %s does not match source %s" % (copied_file_data, test_file_data))
+        os.unlink(local_filename)
+        os.unlink(remote_filename)
+        client.mkdir(client._make_sftp(), remote_dir)
+        self.assertTrue(os.path.isdir(remote_dir))
+        os.rmdir(remote_dir)
+        del client
+        server.join()
 
 if __name__ == '__main__':
     unittest.main()
