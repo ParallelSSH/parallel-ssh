7bfdc820cae105f8a74833e2c457e9e60f3c0c60
Panos <pkittenis@gmail.com>
2014-08-13 13:14:03 +0100

    Updated license to LGPL - v2 only, not later versions. This resolves #5. Bumped version to 0.5

--- pssh.py
+++ pssh.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 
 """Module containing wrapper classes over paramiko.SSHClient
-See SSHClient and ParallelSSHClient"""
+See SSHClient and ParallelSSHClient
+
+Copyright (C) 2014 Panos Kittenis
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, version 2.1.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+"""
 
 import gevent.pool
 from gevent import socket
--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
+"""
+Copyright (C) 2014 Panos Kittenis
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, version 2.1.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
 from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='parallel-ssh',
-      version='0.4',
+      version='0.5',
       description='Wrapper library over paramiko to allow remote execution of tasks. Supports parallel execution on multiple hosts',
       author='Panos Kittenis',
       author_email='pkittenis@gmail.com',
@@ -10,7 +27,7 @@ setup(name='parallel-ssh',
       py_modules = ['pssh'],
       install_requires = ['paramiko', 'gevent'],
       classifiers = [
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
