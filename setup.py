08274be29b1af5b0833c6559080f75e7e456bba0
Panos <pkittenis@gmail.com>
2014-08-13 13:12:08 +0100

    Updated license to LGPL - v2 only, not later versions. This resolves #5

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+"""
+Copyright (C) 2014 Panos Kittenis
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation, version 2.1.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+"""
+
 from distutils.core import setup
 from setuptools import find_packages
 
@@ -10,7 +27,7 @@ setup(name='parallel-ssh',
       py_modules = ['pssh'],
       install_requires = ['paramiko', 'gevent'],
       classifiers = [
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
