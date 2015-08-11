212e2e64f2c46581024881c10a9266ac762db089
Dan <22e889d8@opayq.com>
2015-08-11 17:03:58 +0100

    Updated setup.py for 2to3

--- setup.py
+++ setup.py
@@ -13,8 +13,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 import sys
 
 convert_2_to_3 = {}
