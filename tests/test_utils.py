05cca838fe8f49a499e6e545306499ef8545a533
Panos <zuboci@yandex.com>
2020-09-03 14:19:58 +0100

    Updated utils tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -15,20 +15,10 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-from pssh import utils
 import unittest
-import os
 from logging import NullHandler
-try:
-    from cStringIO import StringIO
-except ImportError:
-    from io import StringIO
-from uuid import uuid4
 
-PKEY_FILENAME = os.path.sep.join([os.path.dirname(__file__), 'test_client_private_key'])
-DSA_KEY_FILENAME = os.path.sep.join([os.path.dirname(__file__), 'test_client_private_key_dsa'])
-ECDSA_KEY_FILENAME = os.path.sep.join([os.path.dirname(__file__), 'test_client_private_key_ecdsa'])
-
+from pssh import utils
 
 class ParallelSSHUtilsTest(unittest.TestCase):
 
@@ -50,22 +40,3 @@ class ParallelSSHUtilsTest(unittest.TestCase):
         self.assertTrue(len([h for h in utils.logger.handlers
                              if not isinstance(h, NullHandler)]) == 1)
         utils.logger.handlers = [NullHandler()]
-
-    def test_loading_key_files(self):
-        for key_filename in [PKEY_FILENAME, DSA_KEY_FILENAME, ECDSA_KEY_FILENAME]:
-            pkey = utils.load_private_key(key_filename)
-            self.assertTrue(pkey, msg="Error loading key from file %s" % (key_filename,))
-            pkey = utils.load_private_key(open(key_filename))
-            self.assertTrue(pkey, msg="Error loading key from open file object for file %s" % (key_filename,))
-        fake_key = StringIO("blah blah fakey fakey key\n")
-        self.assertFalse(utils.load_private_key(fake_key))
-        fake_file = 'fake_key_file'
-        with open(fake_file, 'wb') as fh:
-            fh.write(b'fake key data\n')
-        try:
-            self.assertIsNone(utils.load_private_key(fake_file))
-        finally:
-            os.unlink(fake_file)
-
-    def test_openssh_config_missing(self):
-        self.assertFalse(utils.read_openssh_config('test', config_file=str(uuid4())))
