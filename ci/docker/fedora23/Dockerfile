2d648e6a7a1088a8ba40658af86ab0412387f33f
Panos <zuboci@yandex.com>
2020-09-27 15:06:26 +0100

    Updated manylinux builds. Removed obsolete CI files.

--- ci/docker/fedora23/Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM cdrx/fpm-fedora:23
-
-RUN yum -y install libssh2-devel python-devel python-setuptools git
-RUN curl -sLO https://bootstrap.pypa.io/get-pip.py && python get-pip.py && rm -f get-pip.py && pip install -U setuptools wheel && pip install cython
-RUN pip install ssh2-python gevent paramiko
-
-ENV EMBEDDED_LIB 0
-ENV HAVE_AGENT_FWD 0
--- ci/travis/build-wheels.sh
+++ ci/travis/build-wheels.sh
@@ -1,18 +1,7 @@
 #!/bin/bash -xe
 
-# Compile wheels
-for PYBIN in `ls -1d /opt/python/cp*/bin`; do
-    "${PYBIN}/pip" install -r /io/requirements.txt
-    "${PYBIN}/pip" wheel --no-deps /io/ -w wheelhouse/
-done
-
-# Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" -w /io/wheelhouse/
-done
-
-# Install packages and test
-for PYBIN in `ls -1d /opt/python/cp*/bin`; do
-    "${PYBIN}/pip" install parallel-ssh --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/python" -c 'import pssh.native._ssh2'; "${PYBIN}/python" -c 'import pssh.clients')
-done
+export PYBIN=`ls -1d /opt/python/cp*/bin | tail -2 | head -1`
+$PYBIN/pip install -r /io/requirements.txt
+$PYBIN/pip wheel --no-deps /io/ -w wheelhouse/
+$PYBIN/pip install parallel-ssh --no-index -f /io/wheelhouse
+$PYBIN/python -c "import pssh.clients"
