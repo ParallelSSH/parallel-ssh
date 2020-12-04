5c26931440da96a2e1ee328dde2175c23c1c5287
Panos <zuboci@yandex.com>
2020-12-04 23:09:24 +0000

    Updated examples

---
+++ examples/quickstart.py
@@ -0,0 +1,13 @@
+from pssh.clients import ParallelSSHClient, SSHClient
+
+
+hosts = ['localhost']
+cmd = 'uname'
+
+client = ParallelSSHClient(hosts)
+output = client.run_command(cmd)
+
+for host_out in output:
+    for line in host_out.stdout:
+        print(line)
+print("Host %s: exit_code %s" % (host_out.host, host_out.exit_code))
