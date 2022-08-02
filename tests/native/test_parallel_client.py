23d070a8e2bb1dba5ebbc53e6bdb9c9799b0ea99
simonfelding <45149055+simonfelding@users.noreply.github.com>
2022-08-02 13:57:59 +0200

    look for alias in client instead

--- tests/native/test_parallel_client.py
+++ tests/native/test_parallel_client.py
@@ -957,7 +957,7 @@ class ParallelSSHClientTest(unittest.TestCase):
         self.assertEqual(client._host_clients[0, hosts[0][0]].password, password)
         self.assertEqual(client._host_clients[0, hosts[0][0]].pkey, open(os.path.abspath(self.user_key), 'rb').read())
         for idx, alias_ in enumerate(alias):
-            self.assertEqual(output[idx].alias, alias_)
+            self.assertEqual(client._host_clients[idx, hosts[0][0]].alias, alias_)
         for server in servers:
             server.stop()
 
