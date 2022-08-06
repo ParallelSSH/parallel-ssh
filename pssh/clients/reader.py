8c15c39e8d0263ccaf65841b84f3b5ff62b1ed14
Panos <danst@tutanota.com>
2022-08-06 09:25:04 +0100

    WIP

--- pssh/clients/reader.py
+++ pssh/clients/reader.py
@@ -17,9 +17,18 @@
 
 from io import BytesIO
 
-from gevent import sleep
 from gevent.event import Event
 from gevent.lock import RLock
+
+
+class _Eof(Event):
+    def __init__(self, unread_data):
+        self._unread_data = unread_data
+        Event.__init__(self)
+
+    def set(self):
+        self._unread_data.set()
+        Event.set(self)
 
 
 class ConcurrentRWBuffer(object):
@@ -27,21 +36,22 @@ class ConcurrentRWBuffer(object):
 
     Supports both concurrent reading and writing.
 
-    Iterate on buffer object to read data, yielding greenlet if no data exists
+    Iterate on buffer object to read data, yielding event loop if no data exists
     until self.eof has been set.
 
-    Writers should ``eof.set()`` when finished writing data via ``write``.
+    Writers should call ``ConcurrentRWBuffer.eof.set()`` when finished writing data via ``write``.
 
     Readers can use ``read()`` to get any available data or ``None``.
     """
-    __slots__ = ('_buffer', '_read_pos', '_write_pos', 'eof', '_lock')
+    __slots__ = ('_buffer', '_read_pos', '_write_pos', 'eof', '_lock', '_unread_data')
 
     def __init__(self):
         self._buffer = BytesIO()
         self._read_pos = 0
         self._write_pos = 0
-        self.eof = Event()
         self._lock = RLock()
+        self._unread_data = Event()
+        self.eof = _Eof(self._unread_data)
 
     def write(self, data):
         """Write data to buffer.
@@ -53,14 +63,17 @@ class ConcurrentRWBuffer(object):
             if not self._buffer.tell() == self._write_pos:
                 self._buffer.seek(self._write_pos)
             self._write_pos += self._buffer.write(data)
+            if not self._unread_data.is_set() and self._read_pos < self._write_pos:
+                self._unread_data.set()
 
     def read(self):
-        """Read available data, or return None
+        """Read available data, or return None.
 
         :rtype: bytes
         """
         with self._lock:
             if self._write_pos == 0 or self._read_pos == self._write_pos:
+                self._unread_data.clear()
                 return
             elif not self._buffer.tell() == self._read_pos:
                 self._buffer.seek(self._read_pos)
@@ -73,5 +86,5 @@ class ConcurrentRWBuffer(object):
             data = self.read()
             if data:
                 yield data
-            elif self._read_pos == self._write_pos:
-                sleep(.1)
+            else:
+                self._unread_data.wait()
